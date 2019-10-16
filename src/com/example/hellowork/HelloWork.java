16cb3d6baa414038d0ed1a6c6495a9dc0b717f89
Edinson Galicia <edinson.jgm@gmail.com>
2019-10-16 18:11:12 -0500

    init

---
+++ src/com/example/hellowork/HelloWork.java
@@ -0,0 +1,8 @@
+package com.example.hellowork;
+
+public class HelloWork {
+    public static void main(String[] args) {
+        System.out.println("Test");
+        String cadena = "2" + " " + "3325588";
+    }
+}
