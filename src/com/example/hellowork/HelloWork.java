81a575c68a88a820570d16e19eeacc5a36c300c4
Edinson Galicia <edinson.jgm@gmail.com>
2019-10-16 18:12:47 -0500

    Changes

--- src/com/example/hellowork/HelloWork.java
+++ src/com/example/hellowork/HelloWork.java
@@ -4,5 +4,6 @@ public class HelloWork {
     public static void main(String[] args) {
         System.out.println("Test");
         String cadena = "2" + " " + "3325588";
+        String cadena2 = "3" + " " + "3325588";
     }
 }
